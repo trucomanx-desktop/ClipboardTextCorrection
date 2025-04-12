37784e3ced6f4c5b781d94e43404b56104696aab
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-04-12 03:19:28 -0300

    incremento version

--- src/clipboard_text_correction/about.py
+++ src/clipboard_text_correction/about.py
@@ -1,6 +1,6 @@
 # about.py
 
-__version__ = "0.2.16"
+__version__ = "0.2.17"
 __package__ = "clipboard_text_correction"
 __linux_indicator__ = "clipboard-text-correction-indicator"
 __author__ = "Fernando Pujaico Rivera" 
