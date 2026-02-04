4a0b4fc31767404c5a86bb9ad9303774b332388b
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-02-04 17:24:07 -0300

    ordenando

--- src/program_launcher.py
+++ src/program_launcher.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
-# python3 -m venv venv-temporal
-# source venv-temporal/bin/activate
-# pip install --upgrade pip
-# pip install pyinstaller pyinstaller-hooks-contrib
-# pip install -r requirements.txt
-# cd src
-#
-# ### windows ##
-# python3 -m PyInstaller --onefile --windowed --name clipboard_text_correction --add-data "clipboard_text_correction/icons;icons" --add-data "clipboard_text_correction/data;data" --collect-all PyQt5  program_launcher.py
+'''
+python3 -m venv venv-temporal
+source venv-temporal/bin/activate
+pip install --upgrade pip
+pip install pyinstaller pyinstaller-hooks-contrib
+pip install -r requirements.txt
+cd src
 
+## windows ##
+python3 -m PyInstaller --onefile --windowed --name clipboard_text_correction --add-data "clipboard_text_correction/icons;icons" --add-data "clipboard_text_correction/data;data" --collect-all PyQt5  program_launcher.py
+
+## ubuntu ##
+python3 -m PyInstaller --onefile --windowed --name clipboard_text_correction --add-data "clipboard_text_correction/icons:icons" --add-data "clipboard_text_correction/data:data" --collect-all PyQt5  program_launcher.py
+
+'''
 
 from clipboard_text_correction.indicator import main
 
